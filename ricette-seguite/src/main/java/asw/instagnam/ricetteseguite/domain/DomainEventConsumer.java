f700fc1b1c4a430b07004e06bdbdffc9b2435916
ilaria stocchi <ilaria.stocchi14@gmail.com>
2020-09-02 10:23:38 +0200

    Kafka risolto, ora funzionante. TODO: DB ricette-seguite

--- ricette-seguite/src/main/java/asw/instagnam/ricetteseguite/domain/DomainEventConsumer.java
+++ ricette-seguite/src/main/java/asw/instagnam/ricetteseguite/domain/DomainEventConsumer.java
@@ -1,9 +1,40 @@
 package asw.instagnam.ricetteseguite.domain;
 
-import asw.instagnam.common.api.event.DomainEvent; 
+import asw.instagnam.common.api.event.DomainEvent;  
+import asw.instagnam.connessioniservice.api.event.*;
+import asw.instagnam.ricetteservice.api.event.*;
 
-public interface DomainEventConsumer {
+import org.springframework.stereotype.Service;
 
-	public void onEvent(DomainEvent event);
+import java.util.logging.*;
+
+@Service
+public class DomainEventConsumer {
+
+	private final Logger logger = Logger.getLogger(DomainEventConsumer.class.toString());
+
+	public void onEvent(DomainEvent event) {
+        if (event.getClass().equals(ConnessioneCreatedEvent.class)) {
+			ConnessioneCreatedEvent cce = (ConnessioneCreatedEvent) event;
+			connessioneCreated(cce); 
+		}else if (event.getClass().equals(RicettaCreatedEvent.class)) {
+			RicettaCreatedEvent cce = (RicettaCreatedEvent) event;
+			ricettaCreated(cce); 
+		}else {
+			logger.info("UNKNOWN EVENT: " + event);			
+		}
+	}
 	
+    private void connessioneCreated(ConnessioneCreatedEvent event) {
+		Connessione connessione = new Connessione(event.getId(), event.getFollower(), event.getFollowed());
+		//salvare del DB
+		logger.info("CREATED  CONNESSIONE: " + connessione);
+	}
+	
+    private void ricettaCreated(RicettaCreatedEvent event) {
+		Ricetta ricetta = new Ricetta(event.getId(), event.getTitolo(), event.getAutore());
+		//salvare del DB
+		logger.info("CREATED  RICETTA: " + ricetta);
+	}
+
 }
--- ricette-seguite/src/main/java/asw/instagnam/ricetteseguite/eventlistener/DomainEventListener.java
+++ ricette-seguite/src/main/java/asw/instagnam/ricetteseguite/eventlistener/DomainEventListener.java
@@ -1,9 +1,10 @@
-package asw.instagnam.ricetteseguite.rest.eventlistener;
+package asw.instagnam.ricetteseguite.eventlistener;
 
 import asw.instagnam.common.api.event.DomainEvent; 
-import asw.instagnam.connessioniservice.api.event.*; 
+import asw.instagnam.connessioniservice.api.event.*;
+import asw.instagnam.ricetteservice.api.event.*;
 
-import asw.instagnam.ricetteseguite.domain.ConnessioneDomainEventConsumer;
+import asw.instagnam.ricetteseguite.domain.DomainEventConsumer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -15,19 +16,19 @@ import org.springframework.beans.factory.annotation.Value;
 import java.util.logging.Logger;
 
 @Component 
-public class ConnessioneDomainEventListener implements DomainEventListener {
+public class DomainEventListener {
 
-    private final Logger logger = Logger.getLogger(ConnessioneDomainEventListener.class.toString());
+    private final Logger logger = Logger.getLogger(DomainEventListener.class.toString());
 
     @Autowired
-    private ConnessioneDomainEventConsumer connessioneDomainEventConsumer;
+    private DomainEventConsumer domainEventConsumer;
 
     
-    @KafkaListener(topics = ConnessioniServiceEventChannel.channel)
+    @KafkaListener(topics = {ConnessioniServiceEventChannel.channel, RicetteServiceEventChannel.channel})
     public void listen(ConsumerRecord<String, DomainEvent> record) throws Exception {
         logger.info("EVENT LISTENER: " + record.toString());
         DomainEvent event = record.value();
-		connessioneDomainEventConsumer.onEvent(event); 
+		domainEventConsumer.onEvent(event); 
     }
 
 }
